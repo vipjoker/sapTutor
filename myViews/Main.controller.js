86be9bb04555aa8ca0a1cd1b62f73a0e63cfb19e
tajcig@ya.ru <19900108>
2016-01-20 17:56:31 +0100

    addede paging

---
+++ myViews/Main.controller.js
@@ -0,0 +1,23 @@
+sap.ui.controller('xxx.Main', {
+
+
+    onInit: function () {
+
+    },
+
+  
+    onBeforeRendering: function () {
+
+    },
+
+    onAfterRendering: function () {
+
+    },
+
+    untitled.Main
+    
+    onExit: function () {
+
+    }
+
+});
